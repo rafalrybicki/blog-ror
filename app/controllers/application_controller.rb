c3fa2c46ef7f1343fd2b8cba0759eedcf3f5f974
Rafał Rybicki <rafarybicki@gmail.com>
2019-12-20 16:42:08 +0100

    rename name to username and add validation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username, :password_confirmation)}
 
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :username, :password_confirmation)}
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username,
+         :presence => true,
+         length: { minimum: 4, maximum: 20 },
+         :uniqueness => {
+           :case_sensitive => false
+         }
   has_many :articles
 end
