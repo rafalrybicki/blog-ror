9c8cbc8731883affd00cbf0f4f3df200ca0fd159
Rafał Rybicki <rafarybicki@gmail.com>
2019-12-21 11:20:25 +0100

    change validation for article.rb and seed db

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true, length: { minimum: 3}
+  validates :title, presence: true, length: { in: 3..60 }
   validates :content, presence: true, length: { minimum: 10}
   validates :user_id, presence: true, numericality: { only_integer: true }
 end
