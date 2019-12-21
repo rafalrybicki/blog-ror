e841a5907a28edfadd8be363898112640ac841ed
Rafał Rybicki <rafarybicki@gmail.com>
2019-12-21 10:20:13 +0100

    add validation for article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true, length: { minimum: 3}
+  validates :content, presence: true, length: { minimum: 10}
+  validates :user_id, presence: true, numericality: { only_integer: true }
 end
