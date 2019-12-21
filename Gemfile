aef4c8036e8e7e60573fb7ec33e8c4fad3679de2
Rafał Rybicki <rafarybicki@gmail.com>
2019-12-21 17:45:57 +0100

    add will_paginate

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootstrap'
 gem 'jquery-rails'
 gem 'devise'
+gem 'will_paginate'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.order(created_at: :desc).paginate(:page => params[:page], :per_page => 9)
   end
 
   # GET /articles/1
