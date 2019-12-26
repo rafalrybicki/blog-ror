8a89faf588d6741a635d31ac301dcb296ac90740
Rafał Rybicki <rafarybicki@gmail.com>
2019-12-26 16:44:59 +0100

    add file input for article's image and validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,9 +8,21 @@ class Article < ApplicationRecord
   validates :content, presence: true, length: { minimum: 10}
   validates :user_id, presence: true, numericality: { only_integer: true }
   validates :category_id, presence: true
+  validate :correct_image
 
   def self.search(params)
     articles = Article.where("title ILIKE ? or content ILIKE ?", "%#{params[:q]}%", "%#{params[:q]}%") if params[:q].present?
     articles
+  end
+
+  def correct_image
+    if image.attached? && !image.content_type.in?(%w(image/jpeg image/jpg image/png))
+      errors.add(:image, 'must be a JPEG, JPG or PNG')
+    elsif image.attached? == false
+      errors.add(:image, 'is required')
+    elsif image.byte_size > 1.megabytes
+      errors.add(:image, "musnt not be larger than 1MB")
+    end
+
   end
 end
